982d373bd4d1b89aac8883568656c1a69d0c94d3
Ihor <i.v.levin@student.khai.edu>
2022-04-24 19:25:25 +0300

    Установил firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,42 +1,52 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'geopardy',
+    title: "geopardy",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    "../geopardy/static/css/main.css"
-  ],
+  css: ["../geopardy/static/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    [
+      "@nuxtjs/firebase",
+      {
+        config: {
+          apiKey: "AIzaSyABryU9P4DoJc-CYII6xHHy1Z3Re7uDpSg",
+          authDomain: "jeopardy-8fe27.firebaseapp.com",
+          projectId: "jeopardy-8fe27",
+          storageBucket: "jeopardy-8fe27.appspot.com",
+          messagingSenderId: "182355373009",
+          appId: "1:182355373009:web:32cf20c53dd7fb69774ef0",
+        },
+        services: {
+          firestore: true,
+          storage: true,
+        },
+      },
+    ],
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
--- pages/create.vue
+++ pages/create.vue
@@ -1,13 +1,19 @@
-<template>
-  
-</template>
+<template></template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+        scenario:{
+            name:'',
+            table:[{head:'',body:''}],
+        }
+    };
+  },
+//   mounted() {
+//     this.$fire.firestore.collection("scenarios").doc().set({ name: "Вася" });
+//   },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
